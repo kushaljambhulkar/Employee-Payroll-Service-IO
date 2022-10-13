23cf13cd6b82bb8e613f3fecbec475a44302d85c
Kushal Jambhulkar <kushaljambhulkar82@gmail.com>
2022-10-13 18:59:03 +0530

    add: Performed Diffrence Opretions

---
+++ src/main/java/assigment/employeepayroll/io/FilesDelete.java
@@ -0,0 +1,15 @@
+package assigment.employeepayroll.io;
+
+import java.io.File;
+
+public class FilesDelete {
+	public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
